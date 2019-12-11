929da0ee45610844434d051ca11f68c48d70d529
AkulichVladislav25 <AkulichVladislav25.github.io>
2019-12-11 23:07:05 +0300

    Perfomed using while and ifs. Than test witn npm. The application get the string, that is written form of number from 1 to 999, and give number that equals this string.

---
+++ task-3/task.js
@@ -0,0 +1,41 @@
+/**
+ *
+ * Your task is to write a function that can take a password string
+ * and parse it into actual password numbers.
+ *
+ * @param {string} password
+ * @return {number}
+ *
+ * Examples
+ *
+ *   ' _  _  _ \n' +
+ *   '  ||_||_|\n' +   => 789
+ *   '  ||_| _|\n'
+ *
+ *   ' _  _  _ \n' +
+ *   '|_ |_   |\n' +   => 567
+ *   ' _||_|  |\n',
+ *
+ *   ' _  _  _ \n' +
+ *   '|_||_ |_ \n' +   => 856
+ *   '|_| _||_|\n',
+ *
+ */
+
+function openCookiesRoom(password) {
+  let i=0;
+  let result=0;
+  while(i<=2){
+   if ((password[1+3*i]==' ')&&(password[10+3*i]=='|'))  result=result+4*Math.pow(10,2-i); else if ((password[1+3*i]==' ')&&(password[10+3*i]==' '))  result=result+Math.pow(10,2-i);
+   if ((password[1+3*i]=='_')&&(password[10+3*i]=='|')&&(password[11+3*i]=='_')&&(password[12+3*i]=='|')&&(password[20+3*i]=='|')&&(password[21+3*i]=='_')&&(password[22+3*i]=='|'))  result=result+8*Math.pow(10,2-i);
+   if ((password[1+3*i]=='_')&&(password[10+3*i]=='|')&&(password[11+3*i]=='_')&&(password[12+3*i]=='|')&&(password[20+3*i]==' ')&&(password[21+3*i]=='_')&&(password[22+3*i]=='|'))  result=result+9*Math.pow(10,2-i);
+   if ((password[1+3*i]=='_')&&(password[10+3*i]=='|')&&(password[11+3*i]=='_')&&(password[12+3*i]==' ')&&(password[20+3*i]=='|')&&(password[21+3*i]=='_')&&(password[22+3*i]=='|'))  result=result+6*Math.pow(10,2-i);
+   if ((password[1+3*i]=='_')&&(password[10+3*i]=='|')&&(password[11+3*i]=='_')&&(password[12+3*i]==' ')&&(password[20+3*i]==' ')&&(password[21+3*i]=='_')&&(password[22+3*i]=='|'))  result=result+5*Math.pow(10,2-i);
+   if ((password[1+3*i]=='_')&&(password[10+3*i]==' ')&&(password[11+3*i]=='_')&&(password[12+3*i]=='|')&&(password[20+3*i]=='|')&&(password[21+3*i]=='_')&&(password[22+3*i]==' '))  result=result+2*Math.pow(10,2-i);
+   if ((password[1+3*i]=='_')&&(password[10+3*i]==' ')&&(password[11+3*i]=='_')&&(password[12+3*i]=='|')&&(password[20+3*i]==' ')&&(password[21+3*i]=='_')&&(password[22+3*i]=='|'))  result=result+3*Math.pow(10,2-i); 
+   if ((password[1+3*i]=='_')&&(password[10+3*i]==' ')&&(password[11+3*i]==' ')&&(password[12+3*i]=='|')&&(password[20+3*i]==' rr')&&(password[21+3*i]==' ')&&(password[22+3*i]=='|'))  result=result+7*Math.pow(10,2-i);
+   i++;
+  }
+  return result;
+}
+module.exports = { openCookiesRoom };
